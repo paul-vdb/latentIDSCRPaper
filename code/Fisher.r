8aad8ba645d1f1c15614bfb962445eb25624668d
paul-vdb <paul.vandambates@gmail.com>
2022-11-01 11:37:26 -0700

    Initial commit

---
+++ code/Fisher.r
@@ -0,0 +1,559 @@
+#################################
+## Latent ID SCR
+## Fisher Example
+#################################
+library(sp)
+library(coda)
+library(raster)
+library(nimble)
+library(nimbleSCR)
+library(ggplot2)
+
+source("NimbleFunctions.R")
+
+## Load the Fisher data
+###--- set directory and load files
+load("../data/FisherData.Rda")
+
+buffer <- 6
+traps <- fisher.data[["traps"]]
+
+xlim <- range(traps[,1])+c(-buffer,buffer)
+ylim <- range(traps[,2])+c(-buffer,buffer)
+area <- diff(xlim)*diff(ylim)/100
+
+obs <- fisher.data[["observations"]]
+collars <- fisher.data$collarInfo
+
+## Start: January 20, 2016
+start.date <- as.Date("01-01-2016", format = "%d-%m-%Y")
+end.date <- as.Date("05-03-2016", format = "%d-%m-%Y")
+StudyPeriod <- as.numeric(end.date - start.date)
+
+## 207 detections.
+obs <- fisher.data[["observations"]]
+obs <- obs[as.Date(obs$DateTime) >= start.date & end.date > as.Date(obs$DateTime),]
+nrow(obs)
+
+omega <- obs$TrapNumber ## Trap Mark
+M <- 400		# Data augmentation super pop
+J <- nrow(traps)
+
+## Chandler and Royle Spatial Count model, Algorithm 2
+## Same as MPP marginalized over all ID. 
+counts <- numeric(J)
+for(j in 1:J)
+{
+	counts[j] <- sum(obs$TrapNumber == j)
+}
+
+Model1 <- nimbleCode( {
+	# Priors:
+	sigma ~ dunif(0, 50)
+	lambda ~ dunif(0, 20)
+	psi ~ dbeta(1,1)
+	tau2 <- 1/(2*sigma^2)
+	
+	for(k in 1:M){
+		z[k] ~ dbern(psi)
+		X[k,1] ~ dunif(xlim[1], xlim[2])
+		X[k,2] ~ dunif(ylim[1], ylim[2])
+		d2[k,1:J] <- pow(X[k,1] - traps[1:J,1], 2) + pow(X[k,2] - traps[1:J,2], 2)
+		hkj[k,1:J] <- lambda*exp(-d2[k,1:J] * tau2 )*z[k]
+	}
+
+	### Model for Observation Process:
+	for(j in 1:J){
+		Hj[j] <- sum(hkj[1:M,j])*Time
+		counts[j] ~ dpois(Hj[j])
+	}
+	
+	N <- sum(z[1:M])
+	D <- N/area
+} )
+
+# Initialize model:
+#-------------------------
+init1 <- function(){
+	lambda <- runif(1, 0.1, 1)
+	sigma <- runif(1, 1, 5)
+	X <- cbind(runif(M, xlim[1], xlim[2]), 
+			  runif(M, ylim[1], ylim[2]))
+	d2 <- t(apply(X, 1, FUN = function(x){(x[1] - traps[,1])^2 + (x[2] - traps[,2])^2}))
+	hkj <- exp(-d2/(2*sigma^2))
+	psi <- rbeta(1,1,1)
+	z <- rbinom(M, prob=psi, size = 1)
+	z[1:nrow(traps)] <- 1
+	X[1:nrow(traps),] <- as.matrix(traps) + cbind(rnorm(nrow(traps), 0, 0.1), rnorm(nrow(traps), 0, 0.1))
+	list(
+		lambda = lambda,
+		sigma = sigma,
+		psi = psi,
+		X = X,
+		z = z
+    )
+}
+
+constants1 <- list(
+    J = nrow(traps),
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = StudyPeriod,
+    M = M,
+	area = diff(xlim)*diff(ylim)/100
+	)
+
+data1 <- list(
+	z =  rep(NA, M),
+	counts = counts
+	)
+
+Rmodel <- nimbleModel(Model1, constants1, data1, inits = init1())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D'))
+
+# Use a block update on locations. Saves time.
+conf$removeSamplers('X')
+for(i in 1:M)	{
+	conf$addSampler(target = paste0('X[', i, ', 1:2]'), 
+		type = 'RW_block', silent = TRUE)	# Adaptive is okay for this model.
+}
+conf$removeSamplers('sigma')
+conf$addSampler(target = 'sigma', 
+		type = 'slice', silent = TRUE)
+
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+out1 <- runMCMC(Cmcmc, niter = 100000, nburnin = 40000, nchains = 3, 
+	thin = 1, inits = list(init1(), init1(), init1()), samplesAsCodaMCMC = TRUE)
+
+# plot(out1[,c("sigma", "lambda", "psi")])
+# plot(out1[,c("sigma", "N", "D")])
+# summary(out1)
+# effectiveSize(out1)
+
+## Marked Poisson process model incorporating the 
+## latent ID variable as unknown. 
+##------------------------------------------------
+ModelMPP <- nimbleCode({
+    lambda ~ dunif(0, 20)   # Detection rate at distance 0
+    psi ~ dbeta(1, 1)       # prior on data augmentation bernoulli vec.
+    sigma ~ dunif(0, 50)	# Now the prior is directly on sigma to be consistent with literature.
+    tau2 <- 1/(2*sigma^2)	# Just avoid that extra computation for each animal...
+
+    for(k in 1:M) {
+        ## Standard SCR data augmentation, animal in pop, loc and dist to traps.
+		z[k] ~ dbern(psi)
+        X[k, 1] ~ dunif(xlim[1], xlim[2])
+        X[k, 2] ~ dunif(ylim[1], ylim[2])
+        d2[k,1:J] <- (X[k,1]-traps[1:J,1])^2 + (X[k,2]-traps[1:J,2])^2
+
+		## Now the rate for the Poisson process:
+		## Don't put lambda here for efficiency of updates.
+        pkj[k,1:J] <- exp(-d2[k,1:J]*tau2)
+        ## Hazard rate for animal across all traps.
+		Hk[k] <- sum(pkj[k,1:J])*Time*lambda*z[k]
+		zeros[k] ~ dpois(Hk[k])
+   }
+
+	pID[1:M] <- z[1:M]*lambda	# Puts a prior on ID of z==1, adds lambda to the detections likelihood for efficiency.
+
+    # Trap history model.
+    # and unobserved animal ID.
+    for(i in 1:n_obs) {
+        # Trap probability given ID: Returns pkj[ID_i, omega_i]
+        omega[i] ~ dTrap(p = pkj[1:M, 1:J], ID = ID[i])
+		# ID probability, returns pID[ID_i], does not check for proper probabilities.
+		ID[i] ~ dID(pID = pID[1:M])	
+    }
+	
+    # Estimated population size
+    N <- sum(z[1:M])
+	D <- N/area
+})
+
+# Run the same model from van Dam-Bates et al. Marked Poisson Process on fisher.
+#----------------------------------------------------------------------
+constants.mpp <- list(
+    J = J,
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = StudyPeriod,
+    M = M,
+    n_obs = length(omega),
+	area = diff(xlim)*diff(ylim)/100
+	)
+
+data.mpp <- list(
+    zeros = rep(0, M),
+	z =  rep(NA, M),
+	ID = rep(NA, length(omega)),
+	omega = omega	
+)
+
+# Need to initialize this model as the stochastic node for ID is kind of wrong...
+inits.mpp <- function(){
+	lambda <- runif(1, 0.1, 1)
+	sigma <- runif(1, 1, 2)
+	X <- cbind(runif(M, xlim[1], xlim[2]), 
+			  runif(M, ylim[1], ylim[2]))
+	d2 <- t(apply(X, 1, FUN = function(x){(x[1] - traps[,1])^2 + (x[2] - traps[,2])^2}))
+	hkj <- exp(-d2/(2*sigma^2))
+	psi <- rbeta(1,1,1)
+	z <- rbinom(M, size = 1, prob = psi)
+	ID <- do.call('c', lapply(omega, FUN = function(x) {sample(1:M, 1, prob = z*hkj[,x])}))
+	list(
+		lambda = lambda,
+		sigma = sigma,
+		psi = psi,
+		X = X,
+		z = z,
+		ID = ID
+    )
+}
+
+## Latent ID SCR as MPP
+######################################
+Rmodel <- nimbleModel(ModelMPP, constants.mpp, data.mpp, inits = inits.mpp())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D', 'ID'))
+
+# Use a block update on locations. Saves time.
+# Turn off adaptive sampling and fix the scale of the sampler to something reasonable.
+conf$removeSamplers('X')
+for(i in 1:M){
+	conf$addSampler(target = paste0('X[', i, ', 1:2]'), 
+		type = 'myJAM', silent = TRUE, 
+		control = list(scale = 0.5, xlim = xlim, ylim = ylim, temp = 0.2))
+}
+
+conf$removeSamplers('sigma')
+conf$addSampler(target = 'sigma', 
+		type = 'slice', silent = TRUE, control = list(adaptive = FALSE, scaleWidth = 0.5))	
+
+## Optimized z sampler, it is a small change from standard as it does a check if ID is matched.
+conf$removeSamplers('z')
+conf$addSampler('z', type = 'myBinary', scalarComponents = TRUE)
+## van Dam-Bates categorical sampler, although Nimble has updated their categorical function so they are very similar.
+conf$removeSamplers('ID')
+conf$addSampler('ID', type = 'myCategorical', scalarComponents = TRUE, control = list(M = M))
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+out2 <- runMCMC(Cmcmc, niter = 100000, nburnin = 40000, nchains = 3, 
+	thin = 1, inits = list(inits.mpp(), inits.mpp(), inits.mpp()), samplesAsCodaMCMC = TRUE)
+
+# plot(out2[, c('sigma', 'D')])
+# plot(out2[, c('lambda', 'N')])
+# summary(out2[, c('lambda, 'sigma', 'N', 'D')])
+
+####################################
+# Model 3: Sex as a covariate
+####################################
+Model3 <- nimbleCode({
+    lambda ~ dunif(0, 20)
+    sigma ~ dunif(0, 50)
+    tau2 <- 1/(2*sigma^2)
+
+    psi ~ dbeta(1, 1)      	# prior on data augmentation Bernoulli vec.
+
+	psex ~ dbeta(1, 1)		# Sex Ratio 
+
+	for(k in 1:M) {
+		z[k] ~ dbern(psi)
+		sex[k] ~ dbern(psex)
+		X[k, 1] ~ dunif(xlim[1], xlim[2])
+		X[k, 2] ~ dunif(ylim[1], ylim[2])
+		d2[k,1:J] <- (X[k,1]-traps[1:J,1])^2 + (X[k,2]-traps[1:J,2])^2
+		pkj[k,1:J] <- exp(-d2[k,1:J]*tau2)
+
+		# Hazard rate for animal across all traps.
+		Hk[k] <- sum(pkj[k,1:J])*Time*z[k]*lambda
+		zeros[k] ~ dpois(Hk[k])
+	}
+
+	pID[1:M] <- z[1:M]*lambda
+
+    ## Trap history model and unobserved animal ID.
+    for(i in 1:n_obs) {
+		# Hard match/no match info.
+		pSex[i] <- (sex[ID[i]]+1) == obsSex[i] | obsSex[i] == 0
+		keep[i] ~ dbern(pSex[i])
+		
+		# Trap Prob.
+        omega[i] ~ dTrap(p = pkj[1:M, 1:J], ID = ID[i])
+
+		# ID prior based on z, with lambda mixed in.
+		ID[i] ~ dID(pID = pID[1:M])
+    }
+	
+    # Predicted population size
+    N <- sum(z[1:M])
+	D <- N/area
+})
+
+obsSex <- obs$sex
+constants3 <- list(
+    J = J,
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = StudyPeriod,
+    M = M,
+    n_obs = length(omega),
+	area = diff(xlim)*diff(ylim)/100,
+	obsSex = obsSex
+)
+
+data3 <- list(
+    zeros = rep(0, M),
+    omega = omega,
+	keep = rep(1, length(omega)),
+	z =  rep(NA, M),
+	ID = rep(NA, length(omega)),
+	sex = rep(NA, M)	# Note 0 is male and 1 is female.
+)
+
+# Need to fully initialize because I haven't implemented a random generator distribution for dID()...
+# Bit of a pain to make sure we match sex and collar correctly.
+init3 <- function(){
+	N <- floor(runif(1, 1, M/2))
+	psi <- rbeta(1, N, M-N)	# NA inits...	
+	lambda <- runif(1, 0.1, 1)
+	psex <- rbeta(1, 1, 1)	# Based on collared...
+	sigma <- runif(1, 2, 4)
+	X <- cbind(runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2]))
+	d2 <- t(apply(X, 1, FUN = function(x){(x[1] - traps[,1])^2 + (x[2] - traps[,2])^2}))
+	hkj <- exp(-d2/(2*sigma^2))
+	sex <- rbinom(M, size = 1, prob = psex)
+	ID <- do.call('c',lapply(1:length(omega), FUN = function(x){sample(1:M, 1, prob = hkj[1:M,omega[x]]*(sex+1 == obs$sex[x] |  obs$sex[x] == 0))}))
+	Hk <- rowSums(hkj)*StudyPeriod*lambda
+	p <- exp(-Hk)*psi/(exp(-Hk)*psi + (1-psi))
+	z <- rbinom(M, size = 1, prob=p)
+	z[ID] <- 1
+	list(
+		lambda = lambda,
+		sigma = sigma,
+		psi = psi,
+		X = X,
+		z = z,
+		ID = ID,
+		sex = sex,
+		psex = psex
+    )
+}
+
+###################################
+## Run the Sex fisher model MCMC
+###################################
+Rmodel <- nimbleModel(Model3, constants3, data3, inits = init3())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D', 'ID', 'z', 'sex', 'psex')) # 'psex', 'sex',
+
+conf$removeSamplers('X')
+for(i in 1:M){
+	conf$addSampler(target = paste0('X[', i, ', 1:2]'), 
+		type = 'myJAM', silent = TRUE, 
+		control = list(scale = 0.5, xlim = xlim, ylim = ylim, temp = 0.2))
+}
+
+conf$removeSamplers('sigma')
+conf$addSampler(target = 'sigma', 
+		type = 'slice', silent = TRUE, control = list(adaptive = FALSE, scaleWidth = 0.5))	
+
+# Optimized z sampler
+conf$removeSamplers('z')
+conf$addSampler('z', type = 'myBinary', scalarComponents = TRUE)
+
+conf$removeSamplers('ID')
+conf$addSampler('ID', type = 'myCategorical', scalarComponents = TRUE, control = list(M = M))
+
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+# The full run...
+out3 <- runMCMC(Cmcmc, niter = 100000, nburnin = 40000, nchains = 3, 
+	thin = 1, inits = list(init3(), init3(), init3()), samplesAsCodaMCMC = TRUE)
+
+# plot(out3[, c('sigma', 'lambda', 'psex')])
+# plot(out3[, c('D', 'N')])
+# summary(out3[, c('lambda, 'sigma', 'psex', 'N', 'D')])
+
+####################################
+## Model 4: Sex and collar as covariates
+####################################
+Model4 <- nimbleCode({
+	
+    lambda ~ dunif(0, 20)
+
+	# Home range parameter m=0/f=1:
+    sigma ~ dunif(0, 50)	
+
+	# convert now to make it simpler in half-normal computation
+    tau2 <- 1/(2*sigma^2)	
+
+    psi ~ dbeta(1, 1)      # prior on data augmentation bernoulli vec.
+
+	psex ~ dbeta(1, 1)
+
+	# For the collared individuals, we observe the sex too.
+	# So we have observed sex 1:14 and z = 1 for those animals.
+	for(k in 1:M) {
+		z[k] ~ dbern(psi)
+		sex[k] ~ dbern(psex)
+		
+		X[k, 1] ~ dunif(xlim[1], xlim[2])
+		X[k, 2] ~ dunif(ylim[1], ylim[2])
+		d2[k,1:J] <- (X[k,1]-traps[1:J,1])^2 + (X[k,2]-traps[1:J,2])^2
+
+		pkj[k,1:J] <- exp(-d2[k,1:J]*tau2)*lambda	## In this case I've put lambda here... this was for sex specific rates if necessary.
+		# Hazard rate for animal across all traps.
+		Hk[k] <- sum(pkj[k,1:J])*Time*z[k]
+
+		# Zero count for all.
+		zeros[k] ~ dpois(Hk[k])
+	}
+
+	# Prior on animal ID is just 1|z = 1 or 0|z = 0. 
+	# The reason is in homogeneous time things cancel nicely and this is easiest.
+	pID[1:M] <- z[1:M]
+
+    # Trap history model.
+    # and unobserved animal ID.
+    for(i in 1:n_obs) {
+		# Hard match/no match info.
+		pSex[i] <- (sex[ID[i]]+1) == obsSex[i] | obsSex[i] == 0
+		# We add matchCollar here to make sure collar matches.
+		pMatch[i] <- matchCollar[ID[i],i]*pSex[i]
+		# Hard 1/0 for animal match to sex and collar.
+		keep[i] ~ dbern(pMatch[i])
+		
+		# Trap Prob:
+        omega[i] ~ dTrap(p = pkj[1:M, 1:J], ID = ID[i])
+
+		# ID prior based on z:
+		ID[i] ~ dID(pID = pID[1:M])
+    }
+	
+    # Predicted population size
+    N <- sum(z[1:M])
+	D <- N/area
+})
+
+# Process collar information per animal.
+########################################
+collars <- fisher.data$collarInfo
+matchCollar <- matrix(1, nrow = M, ncol = length(omega))
+collars$Sex <- 2 - (collars$Sex == "M")
+dates <- obs$DateTime
+dates <- as.Date(dates)
+obsCollar <- obs$collar
+
+# Process collar matrix:
+for(i in 1:length(omega))
+{
+	indx.match <- collars$Start < dates[i] & collars$Finish >= dates[i]
+	if(obsCollar[i] == 1)
+	{
+		matchCollar[(nrow(collars)+1):M,i] <- 0
+		matchCollar[1:nrow(collars), i] <- indx.match*1
+	}else{
+		if(obsCollar[i] == 2) matchCollar[1:nrow(collars),i] <- (1-indx.match)		
+	}
+	if(dates[i] > as.Date("2016-01-25", format = "%Y-%m-%d")){
+		matchCollar[which(collars$FisherID == "M02"),i] <- 0
+	}
+}
+
+constants4 <- list(
+    J = J,
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = StudyPeriod,
+    M = M,
+    n_obs = length(omega),
+	area = diff(xlim)*diff(ylim)/100,
+	obsSex = obs$sex,
+	obsMark = obs$collar
+	)
+
+data4 <- list(
+    zeros = rep(0, M),
+    omega = omega,
+	keep = rep(1, length(omega)),
+	z =  c(rep(1, nrow(collars)), rep(NA, M-nrow(collars))),
+	ID = rep(NA, length(omega)),
+	sex = c(collars$Sex - 1, rep(NA, M - nrow(collars))),	# Note 0 is male and 1 is female.
+	matchCollar = matchCollar	# needs to be data as we can't legally dynamically index constants...
+)
+
+init4 <- function(){
+	lambda <- runif(1, 0.1, 1)
+	psex <- rbeta(1, 9, 14)	# Based on collared...
+	sigma <- runif(1, 2, 4)
+	X <- cbind(runif(M, xlim[1], xlim[2]), 
+			  runif(M, ylim[1], ylim[2]))
+	d2 <- t(apply(X, 1, FUN = function(x){(x[1] - traps[,1])^2 + (x[2] - traps[,2])^2}))
+	hkj <- exp(-d2/(2*sigma^2))
+	sexCollar <- collars$Sex-1
+	sex <- c(sexCollar, rbinom(M- nrow(collars), size = 1, prob = psex))
+	ID <- do.call('c',lapply(1:length(omega), 
+		FUN = function(x){sample(1:M, 1, prob = hkj[,omega[x]]*matchCollar[,x]*(obs$sex[x] == 0 | (obs$sex[x] - 1) == sex))}))
+	sex[1:nrow(collars)] <- NA
+	z <- rep(0, M)
+	z[ID] <- 1
+	z[1:nrow(collars)] <- NA
+	psi <- rbeta(1, sum(z, na.rm = TRUE) + nrow(collars), M - sum(1-z, na.rm = TRUE))	# NA inits...
+	list(
+		lambda = lambda,
+		sigma = sigma,
+		psi = psi,
+		X = X,
+		z = z,
+		ID = ID,
+		sex = sex,
+		psex = psex
+    )
+}
+
+###################################
+# Chandler and Royle Sampler:
+###################################
+Rmodel <- nimbleModel(Model4, constants4, data4, inits = init4())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('sigma', 'lambda', 'psi', 'N', 'D', 'ID', 'psex', 'sex'))
+
+conf$removeSamplers('X')
+for(i in 1:M){
+	conf$addSampler(target = paste0('X[', i, ', 1:2]'), 
+		type = 'myJAM', silent = TRUE, control = list(scale = 1, xlim = xlim, ylim = ylim, temp = 0.2))
+}
+
+conf$removeSamplers('sigma')
+# sigma slice sampling.
+conf$addSampler(target = 'sigma', 
+		type = 'slice', silent = TRUE, scalarComponents = TRUE, control = list(adaptive = FALSE, scaleWidth = 0.5))		
+
+# Optimized z sampler
+conf$removeSamplers('z')
+conf$addSampler('z', type = 'myBinary', scalarComponents = TRUE)
+
+conf$removeSamplers('ID')
+conf$addSampler('ID', type = 'myCategorical', scalarComponents = TRUE, control = list(M = M))
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+# The full run...
+out4 <- runMCMC(Cmcmc, niter = 100000, nburnin = 40000, nchains = 3, 
+	thin = 1, inits = list(init4(), init4(), init4()), samplesAsCodaMCMC = TRUE)
+
+plot(out4[, c("N", "D")])
+plot(out4[, c("sigma", "lambda", "psex")])
---
+++ code/Frogs.r
@@ -0,0 +1,305 @@
+#################################
+# Latent ID ASCR
+# Frog Example with Time of Arrival.
+#################################
+library(sp)
+library(coda)
+library(raster)
+library(nimble)
+library(nimbleSCR)
+library(coda)
+library(ggplot2)
+library(secr)
+library(ascr)
+
+source("NimbleFunctions.R")
+
+load("../data/stacked-lightfooti.Rdata")
+
+# Change the mask to finer resolution for the CD MLE.
+traps2 <- convert.traps(traps)
+mask <- make.mask(traps = traps2, buffer = 15, spacing = 0.2, type = "trapbuffer")
+A <- attr(mask, "area")
+mask <- as.matrix(mask)
+attr(mask, "area") <- A
+attr(mask, "buffer") <- 15
+
+###########
+# Latent ID ASCR from Stevenson 2021
+###########
+inits <- function(){
+	ID <- numeric(length(occ))
+	ID[occ == 1] <- 1:sum(occ == 1)
+	ID[occ == 2] <- 1:sum(occ == 2)
+	z <- matrix(0, ncol = 2, nrow = M)
+	z[ID[occ==1],1] <- 1
+	z[ID[occ==2],2] <- 1
+    p <- runif(1, 0.1, 0.3)
+	lambda = runif(1, 0.1, 2)
+	sigma = runif(1, 3, 5)
+	g0 = runif(1,1,10)
+
+	dmask2 <- t(apply(mask, 1, FUN = function(x){(traps[,1]-x[1])^2 + (traps[,2] - x[2])^2}))
+	pkj <- (1-exp(-g0*exp(-dmask2/(2*sigma^2))))
+	panimal <- apply(pkj, 1, FUN = function(x){colSums(log(x)%*%t(capt) + log(1-x)%*%t(1-capt))})
+	X <- array(c(runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2]), 
+		runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2])), c(M,2,2))		
+	
+	for(k in 1:2){
+		for(i in 1:M){
+			if(sum(ID[occ == k] == i) == 0) next;
+			if(sum(ID[occ == k] == i) == 1) pID <- panimal[which(ID[occ == k] == i), ]
+			if(sum(ID[occ == k] == i) > 1) pID <- colSums(panimal[which(ID[occ == k] == i), ])
+			mpt <- sample(ncol(panimal), 1, prob = exp(pID))
+			X[i,,k] <- mask[mpt,]
+		}
+	}
+
+	list(
+        lambda = lambda,
+        psi = p,
+        sigma = sigma,
+		sigmatoa = runif(1, 0.01, 1),
+		g0 = g0,
+		X = X,
+        ID = ID,
+		z = z
+    )
+}
+
+code <- nimbleCode({
+	lambda ~ dunif(0, 10) # Detection rate at distance 0
+	psi ~ dbeta(1, 1)      # Prior on data augmentation bernoulli vec.
+	sigma ~ dunif(0, 10)	# Now the prior is directly on sigma to be consistent with literature.
+	tau2 <- 1/(2*sigma^2)
+	sigmatoa ~ dunif(0, 1)
+	g0 ~ dunif(0, 20)
+	for(v in 1:n_occ) {
+		for(k in 1:M) {
+			z[k,v] ~ dbern(psi)
+			X[k, 1, v] ~ dunif(xlim[1], xlim[2])
+			X[k, 2, v] ~ dunif(ylim[1], ylim[2])
+			d2[k,1:J, v] <- (X[k,1,v]-traps[1:J,1])^2 + (X[k,2,v]-traps[1:J,2])^2
+			expTime[k, 1:J, v] <- sqrt(d2[k,1:J,v])/nu
+			pkj[k,1:J,v] <- (1-exp(-g0*exp(-d2[k,1:J,v]*tau2)))
+			# Hazard rate for animal across all traps.
+			Hk[k,v] <-(1-prod(1-pkj[k,1:J,v]))*lambda*Time*z[k,v]
+			zeros[k,v] ~ dpois(Hk[k,v])
+		}
+		# Predicted population size for each occ
+		N[v] <- sum(z[1:M,v])
+	}
+	
+	pID[1:M, 1:n_occ] <- z[1:M,1:n_occ]*lambda
+	
+	# Trap history model.
+	# and unobserved animal ID.
+	for(i in 1:n_obs) {
+		# Bernoulli capture history for each call that depends on ID
+		y[i,1:J] ~ dbinom_vector(size = trials[1:J], pkj[ID[i],1:J, occ[i]])
+		# Time of arrival, depends on which traps actually recorded it.
+		toa[i, 1:J] ~ dnorm_vector_marg(mean = expTime[ID[i], 1:J, occ[i]], sd = sigmatoa, y = y[i,1:J])
+		# The likelihood needs to be multiplied by lambda for each detection and
+		# I need ID to be a stochastic node. 2 birds...
+		ID[i] ~ dID(pID = pID[1:M, occ[i]])
+	}
+
+	# Derived Variables.
+	EN <- psi*M
+	D <- EN/area*10000
+})
+
+xlim <- range(mask[,1])
+ylim <- range(mask[,2])
+area <- diff(xlim)*diff(ylim)
+toa <- capt.all$toa
+capt <- capt.all$bincapt[,1:6]
+tmin <- apply(toa, 1, max)
+occ <- 1+(tmin > 1200)
+ID <- capt.all$bincapt[, 7]
+ID <- as.integer(as.factor(ID))
+IDBen <- ID
+
+# Constants:
+M <- 200
+nu <- 330
+J <- nrow(traps)
+n <- nrow(capt)
+Time <- 30
+
+constants <- list(
+    J = J,
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = Time,
+    M = M,
+    n_obs = nrow(capt),
+	trials = rep(1, J),
+	nu = nu,
+	area = area,
+	n_occ = 2,
+	occ = occ,
+	mi = rowSums(capt))
+
+data <- list(
+	zeros = cbind(rep(0,M), rep(0,M)),
+    y = capt,
+	toa = toa,
+	z = cbind(rep(NA, M), rep(NA, M)),
+	ID = rep(NA, nrow(capt))
+)
+
+Rmodel <- nimbleModel(code, constants, data, inits = inits())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('psi', 'sigma', 'lambda', 'sigmatoa', 'g0', 'N', 'D', 'ID'))
+
+conf$removeSamplers('X')
+for(v in 1:2){
+	for(i in 1:M) {
+		conf$addSampler(target = paste0('X[', i, ', 1:2,', v, ']'), 
+			type = 'myJAM', silent = TRUE, 
+			control = list(scale = 1, xlim = xlim, ylim = ylim, temp = 0.2, occ = 2))
+	}
+}
+
+conf$removeSamplers('g0')
+conf$addSampler(target = 'g0', 
+		type = 'slice', silent = TRUE, control = list(adaptive = FALSE, scaleWidth = 0.5))		
+
+conf$removeSamplers('z')
+conf$addSampler('z', type = 'myBinary', scalarComponents = TRUE, 
+	control = list('Noccasion' = 2, 'IDoccasion' = occ))
+
+conf$removeSamplers('ID')
+# Sampler from Chandler and Royle 2013
+conf$addSampler('ID', type = 'myCategorical', scalarComponents = TRUE, control = list(M = M))
+
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+out <- runMCMC(Cmcmc, nburnin = 20000, niter = 50000, nchains = 3, 
+	inits = list(inits(), inits(), inits()), samplesAsCodaMCMC = TRUE)	
+
+summary(out)
+
+###########
+# Known ID ASCR from Stevenson 2021
+###########
+initsID <- function(){
+    p <- runif(1, 0.1, 0.3)
+	z <- matrix(rbinom(M*2, 1, p), ncol = 2, nrow = M)
+	z[IDBen[occ==1],1] <- NA
+	z[IDBen[occ==2],2] <- NA
+	lambda = runif(1, 0.1, 2)
+	sigma = runif(1, 3, 5)
+	g0 = runif(1,1,10)
+
+	dmask2 <- t(apply(mask, 1, FUN = function(x){(traps[,1]-x[1])^2 + (traps[,2] - x[2])^2}))
+	pkj <- (1-exp(-g0*exp(-dmask2/(2*sigma^2))))
+	panimal <- apply(pkj, 1, FUN = function(x){colSums(log(x)%*%t(capt) + log(1-x)%*%t(1-capt))})
+	X <- array(c(runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2]), 
+		runif(M, xlim[1], xlim[2]), runif(M, ylim[1], ylim[2])), c(M,2,2))		
+	
+	for(k in 1:2){
+		for(i in 1:M){
+			if(sum(IDBen[occ == k] == i) == 0) next;
+			if(sum(IDBen[occ == k] == i) == 1) pID <- panimal[which(IDBen[occ == k] == i), ]
+			if(sum(IDBen[occ == k] == i) > 1) pID <- colSums(panimal[which(IDBen[occ == k] == i), ])
+			mpt <- sample(ncol(panimal), 1, prob = exp(pID))
+			X[i,,k] <- mask[mpt,]
+		}
+	}
+
+	list(
+        lambda = lambda,
+        psi = p,
+        sigma = sigma,
+		sigmatoa = runif(1, 0.01, 1),
+		g0 = g0,
+		X = X,
+		z = z
+    )
+}
+
+ID <- capt.all$bincapt[, 7]
+IDBen[occ == 1] <- as.integer(as.factor(ID[occ == 1]))
+IDBen[occ == 2] <- as.integer(as.factor(ID[occ == 2]))
+K <- c(max(IDBen[occ == 1]), max(IDBen[occ == 2]))
+
+constants.id <- list(
+    J = J,
+    xlim = xlim,
+    ylim = ylim,
+    traps = traps, 
+    Time = Time,
+    M = M,
+    n_obs = nrow(capt),
+	trials = rep(1, J),
+	nu = nu,
+	area = area,
+	n_occ = 2,
+	occ = occ
+)
+
+data.id <- list(
+	zeros = matrix(0, nrow = M, ncol = 2),
+    y = capt,
+	toa = toa,
+	z = cbind(c(rep(1, K[1]), rep(NA, M-K[1])),
+		c(rep(1, K[2]), rep(NA, M-K[2]))),
+	ID = IDBen
+)
+
+Rmodel <- nimbleModel(code, constants.id, data.id, inits = initsID())
+conf <- configureMCMC(Rmodel)
+conf$setMonitors(c('sigma', 'lambda', 'sigmatoa', 'g0', 'N', 'D'))
+
+conf$removeSamplers('X')
+for(v in 1:2){
+	for(i in 1:M) conf$addSampler(target = paste0('X[', i, ', 1:2,', v, ']'), type = 'RW_block', silent = TRUE)
+}
+
+conf$removeSamplers('g0')
+conf$addSampler(target = 'g0', 
+		type = 'slice', silent = TRUE, control = list(adaptive = FALSE, scaleWidth = 0.5))		
+
+Rmcmc <- buildMCMC(conf)
+Cmodel <- compileNimble(Rmodel)
+Cmcmc <- compileNimble(Rmcmc, project = Rmodel)
+
+out.id <- runMCMC(Cmcmc, nburnin = 20000, niter = 50000, nchains = 3, 
+	inits = list(initsID(), initsID(), initsID()), samplesAsCodaMCMC = TRUE)	
+
+summary(out.id)
+
+############################
+# Now run it with ASCR, 
+# but we need to increase the mask:
+############################
+multi.capt <- list(list("bincapt" = capt[occ==1,], "toa" = toa[occ==1,]), list("bincapt" = capt[occ ==2,], "toa" = toa[occ==2,]))
+ascr.res <- fit.ascr(capt = multi.capt, list(traps, traps), mask = list(mask, mask), 
+			detfn = "hhn", survey.length = c(30, 30))
+# citation("ascr")
+summary(ascr.res)
+confint(ascr.res)
+# Detection function: Hazard halfnormal 
+# Information types: Times of arrival
+ 
+ # Parameters: 
+            # Estimate Std. Error
+# D         1.2489e+02    10.1940
+# lambda0   6.5932e+00     1.0356
+# sigma     2.2916e+00     0.0943
+# sigma.toa 9.6946e-04     0.0001
+# ---                            
+                               
+# esa.1     2.4556e-02     0.0009
+# esa.2     2.4556e-02     0.0009
+# > confint(ascr.res)
+                 # 2.5 %       97.5 %
+# D         1.049065e+02 1.448663e+02
+# lambda0   4.563476e+00 8.622954e+00
+# sigma     2.106834e+00 2.476330e+00
+# sigma.toa 7.833584e-04 1.155556e-03
